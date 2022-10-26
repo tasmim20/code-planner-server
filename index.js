a218492e27863fb5563d270380c3ea02740fa6c3
Tasmim Rahman <tasmimrahman2000@gmail.com>
2022-10-26 19:29:03 +0600

    category added

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = require('./data/courses.json');
+const categories = require('./data/categories.json');
+const courses= require('./data/courses.json');
 
 
 app.get('/', (req, res) => {
     res.send('course API Running');
 });
 
-app.get('/courses-catagories', (req, res) => {
-    res.send(courses)
+app.get('/courses-categories', (req, res) => {
+    res.send(categories);
 });
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    const category_course = courses.filter(course => course.course_id  === id);
+    res.send(category_course);
+})
+
+app.get('/courses/:id', (req, res)=>{
+    const id = req.params.id
+    const selectedCourse = courses.find(course => course._id === id);
+    res.send(selectedCourse);
+} )
 
 app.listen(port, () => {
     console.log('courses Server running on port', port);
